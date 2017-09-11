00f093713ed0a30e5a88ac1aea5d1f9aa4aa1b72
Garrett Hafen <garrett.hafen12@gmail.com>
2017-09-11 17:00:59 -0600

    work from firstbuild

---
+++ BlockBreaker/Assets/Scripts/BackgroundMusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusicPlayer : MonoBehaviour {
+	
+	static BackgroundMusicPlayer instance = null; 
+	
+	void Awake(){
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate destroyed");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
