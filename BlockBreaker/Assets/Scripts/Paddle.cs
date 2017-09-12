ce7f6840a7baff81e48bb7fdea1a134637584fa4
Garrett Rees Hafen <GarrettHafen@ldschurch.org>
2017-09-11 18:45:29 -0600

    added physics to the ball and the paddle, started working on getting the paddle moving

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	float mousePosInBlocks;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//relative mouse position with valuse between 0 and 1
+		mousePosInBlocks = (Input.mousePosition.x) /Screen.width * 16;
+		print (mousePosInBlocks);
+	}
+}
