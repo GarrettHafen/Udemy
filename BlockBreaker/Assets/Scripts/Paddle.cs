3f3af9c5a285c1592c0a98d09b6d482b5fe1b391
Garrett Rees Hafen <GarrettHafen@ldschurch.org>
2017-09-13 07:56:53 -0600

    added block destruction and prefabs and gravity and wall colliders

--- BlockBreaker/Assets/Scripts/Paddle.cs
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -10,8 +10,15 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+
 		//relative mouse position with valuse between 0 and 1
 		mousePosInBlocks = (Input.mousePosition.x) /Screen.width * 16;
-		print (mousePosInBlocks);
+
+		paddlePos.x = Mathf.Clamp (mousePosInBlocks, .5f, 15.5f);
+
+		this.transform.position = paddlePos;
+		;
+
 	}
 }
